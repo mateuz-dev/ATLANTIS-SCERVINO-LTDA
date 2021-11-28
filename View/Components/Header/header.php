1dec0b3049f79e5bd11cae228ecff964e4b95aaf
Daniel25778 <danielwilsonca94@gmail.com>
2021-11-28 00:09:01 -0300

    inicio do desenvolvimento do header

--- View/Components/Header/header.php
+++ View/Components/Header/header.php
@@ -7,7 +7,22 @@
 </head>
 <body>
     <header>
-    
+    <nav>
+        <a class="logo" href="/">Tiger Codes</a>
+        <div class="mobile-menu">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+        <ul class="nav-list">
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main></main>
     </header>
 </body>
 </html>
