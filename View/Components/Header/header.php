3b1beda70e6b5baa2d50e278f284e4c6f3134726
mateuz-dev <niremalu.mateus@gmail.com>
2021-11-28 23:04:34 -0300

    Header desktop finalizado

--- View/Components/Header/header.php
+++ View/Components/Header/header.php
@@ -1,26 +1,97 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <!-- FONT IMPORTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lustria&family=Montserrat&display=swap" rel="stylesheet">
+
+    <!-- CSS IMPORTS -->
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="../../style-global.css">
+
+    <title>Header</title>
 </head>
+
+
 <body>
     <header>
-    <nav>
-        <a class="logo" href="/">SCERVINO</a>
-        <div class="mobile-menu">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
-        </div>
-        <ul class="nav-list">
-          <li><a href="#">Nossa historia</a></li>
-          <li><a href="#"></a></li>
-        </ul>
-      </nav>
-    </header>
-    <main></main>
+        <a href="">
+            <div id="div-title">
+                <img src="./images/logo.png">
+                <h1>SCERVINO</h1>
+            </div>
+        </a>
+
+        <nav>
+            <ul id="ul-menu-options">
+                <li> <a href="">Nossa História</a> </li>
+
+                <li id="li-catalog"> Catálogo <img src="./images/arrow-icon.png" id="arrow">
+
+                    <ul id="ul">
+                        <ul id="ul-triangle"><img src="./images/triangle-icon.png"></ul>
+    
+                        <ul id="ul-categories">
+                            <li> 
+                                <a href="">
+                                    <img src="./images/category-icon.png">
+                                    Nome da Categoria
+                                </a> 
+                            </li>
+                            <li> 
+                                <a href="">
+                                    <img src="./images/category-icon.png">
+                                    Nome da Categoria
+                                </a> 
+                            </li>
+                            <li> 
+                                <a href="">
+                                    <img src="./images/category-icon.png">
+                                    Nome da Categoria
+                                </a> 
+                            </li>
+                            <li> 
+                                <a href="">
+                                    <img src="./images/category-icon.png">
+                                    Nome da Categoria
+                                </a> 
+                            </li>
+                            <li> 
+                                <a href="">
+                                    <img src="./images/category-icon.png">
+                                    Nome da Categoria
+                                </a> 
+                            </li>
+                            <li> 
+                                <a href="">
+                                    <img src="./images/category-icon.png">
+                                    Nome da Categoria
+                                </a> 
+                            </li>
+                        </ul>
+                    </ul>
+
+                </li>
+                
+                <form action="">
+                    <img src="./images/search-icon.png">
+                    <input type="text" name="" id="" placeholder="Buscar">
+                </form>
+            </ul>
+
+            <ul id="ul-user-options">
+                <a href=""> <img src="./images/cart-icon.png" alt="Carrinho"> </a>
+                <a href=""> <img src="./images/user-icon.png" alt="Login/Cadastrar"> </a>
+            </ul>
+        </nav>
+
     </header>
 </body>
+
 </html>
