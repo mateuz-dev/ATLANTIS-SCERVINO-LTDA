8f5e72770be86a04202b8d91b1de637c4310e409
GabrielTSR <gabrielalvesrocha22@gmail.com>
2021-12-03 11:07:07 -0300

    components js

---
+++ Front-end/View/Components/Footer/footer.js
@@ -0,0 +1,54 @@
+document.querySelector('#footer').innerHTML =
+    `<div id="div-top">
+
+<div id="div-title">
+    <img src="../../images/logo-branco.png">
+    <h1 class="title">SCERVINO</h1>
+    <p>DESDE 2012</p>
+</div>
+
+
+<div id="div-comunication">
+
+    <div id="div-social-media">
+        <p class="info-title">Siga-nos</p>
+
+        <a href="https://www.facebook.com/atlantisscervino/" target="blank">
+            <img src="images/facebook_icon.png">
+            <p>Facebook</p>
+        </a>
+
+        <a href="https://www.instagram.com/invites/contact/?i=1id1s1kldwssm&utm_content=n0z2bhl" target="blank">
+            <img src="images/instagram_icon.png">
+            <p>Instagram</p>
+        </a>
+    </div>
+
+
+    <div id="div-contact">
+        <p class="info-title">Contato</p>
+
+        <a href="">
+            <img src="images/telephone_icon.png">
+            <p>4002-8922</p>
+        </a>
+
+        <a href="">
+            <img src="images/email_icon.png">
+            <p>atlantisscervinosustentaveis@gmail.com</p>
+        </a>
+
+        <a href="">
+            <img src="images/whatsapp_icon.png">
+            <p>(11) 9999-9999</p>
+        </a>
+    </div>
+
+</div>
+
+</div>
+
+
+<div id="footer-bottom">
+<p>© Scervino 2021. Todos os direitos reservados</p>
+</div>`
