ee5cca592a87935ce34ee6fc1b8b419eab45ddd4
MATEUS BISPO RAMOS <21192976@CFP127ALUNO.LOCAL>
2021-12-13 10:32:59 -0300

    category slider finished

--- Front-end/View/page-landingPage/script.js
+++ Front-end/View/page-landingPage/script.js
@@ -1,9 +1,19 @@
 "use-strict"
 
-const moveSlider = (direction) =>{
-    if(direction == "MOVE-LEFT"){
-        console.log("MOVE LEFT")
-    } else{
-        console.log("MOVE RIGHT")
-    }
+const moveSliderRight = () =>{
+    divCategories.style.marginRight = "20vw"
+    divCategories.style.marginLeft = "-20vw"
+    divCategories.style.transition = "1s ease-in-out"
 }
+
+const moveSliderLeft = () =>{
+    divCategories.style.marginLeft = "20vw"
+    divCategories.style.marginRight = "-20vw"
+    divCategories.style.transition = "1s ease-in-out"
+}
+
+const divCategories = document.querySelector("#div-categories")
+const sliderRightButton = document.querySelector("#button-landing-page-categories-right")
+sliderRightButton.addEventListener('click', moveSliderRight)
+const sliderLefttButton = document.querySelector("#button-landing-page-categories-left")
+sliderLefttButton.addEventListener('click', moveSliderLeft)
